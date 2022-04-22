ed2ac388e227cf80c017dc7a1550c3dde1184632
Станислав Сбродов <stud0000232373@study.utmn.ru>
2022-04-22 19:27:23 +0500

    Добавил API

---
+++ API.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractsList
+{
+    class API
+    {
+        BaseRepository repository { get; set; }
+
+        User user { get; }
+
+        public MunicipalContract GetContract(int numberApplication)
+        {
+            return repository.GetContract(numberApplication);
+        }
+
+        public List<MunicipalContract> GetContractList(Dictionary<string, string> filters)
+        {
+            return repository.GetContractList(user, filters);
+        }
+
+        public void AddContract(MunicipalContract contract)
+        {
+            repository.AddContract(user, contract);
+        }
+
+        public void UpdateContract(MunicipalContract contract)
+        {
+            repository.UpdateContract(user, contract);
+        }
+
+        public void DeleteContract(List<MunicipalContract> contracts)
+        {
+            repository.DeleteContract(user, contracts);
+        }
+
+        public void SaveFile(MunicipalContract contract, File file)
+        {
+            repository.SaveFile(user, contract, file);
+        }
+
+        public void DeleteFile(MunicipalContract contract)
+        {
+            repository.DeleteFile(user, contract);
+        }
+
+    }
+}
